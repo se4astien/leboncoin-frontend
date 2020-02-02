35be1559e1572d6637d97f5b1cdac931eb16909e
sebv <sebv@sv.home>
2020-02-02 22:18:45 +0100

    update css price date

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,9 +7,9 @@ const Product = props => {
       <Link to={"offer/" + props._id}>
         <div className="product flex">
           <img src={props.pictures[0]} alt={props.title} />
-          <div className="infos flex">
+          <div className="infos">
             <h2>{props.title}</h2>
-            <div className="price-date flex">
+            <div className="price-date">
               <span>{props.price}€</span>
               <span>{props.date}</span>
             </div>
